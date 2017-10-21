60bcce8ae795368f52d2913cc0ab04e1013ff1ab
Kieran Rimmer <kieran.rimmer@servian.com>
2017-10-22 08:35:16 +1100

    class inheritance example included

---
+++ code/classes_simple.py
@@ -0,0 +1,38 @@
+__author__ = 'kieranrimmer'
+
+class A():
+
+    # static variable with default value
+    n_A = 0
+
+    def __init__(self, x = None):
+        """
+        Constructor for A
+        :param x:
+        :return:
+        """
+        # increment static variable
+        A.n_A += 1
+        # declare and initialise instance variable
+        self.x = x
+
+    def print_out(self):
+        print('n_A = ', A.n_A)
+        print('x = ', self.x)
+
+
+
+def do_stuff():
+    a1 = A()
+    a1.print_out()
+    a2 = A('hello world')
+    a2.print_out()
+    a3 = A(55)
+    a3.print_out()
+    for x in [a1, a2, a3]:
+        x.print_out()
+
+if __name__ == '__main__':
+    do_stuff()
+
+
